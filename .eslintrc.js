9b4de91d02c774e7d64c0262f57c9b1e5da09d4a
Sebastián Millán <sebastianmillanlejarde1120@gmail.com>
2023-02-06 21:37:31 -0500

    Agregando el eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    '@next/next/no-img-element': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.ts', '.tsx', '.jsx'] }
+    ],
+    quotes: 'off',
+    'space-before-function-paren': 'off',
+    'no-unused-vars': 'off'
+  }
+}
