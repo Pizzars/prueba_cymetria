b7cf7e32726dc1e76bdba1b17af5a3ad260e4298
Nicolas Aguilar <n.aguilar@uniandes.edu.co>
2023-05-29 12:28:02 -0500

    Standarize http request response

---
+++ src/services/messageCodes.enum.ts
@@ -0,0 +1,7 @@
+export enum MESSAGE_CODES {
+  USER_NOT_FOUND
+}
+
+export const MESSAGE_CODES_TEXTS: Record<MESSAGE_CODES, string> = {
+  [MESSAGE_CODES.USER_NOT_FOUND]: 'El usuario no fue encontrado'
+}
