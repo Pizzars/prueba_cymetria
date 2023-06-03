ce7caa463ffd55a349af04974c02058d1320566b
Juan Jose Ruiz Soto <jyjdajj@gmail.com>
2023-06-03 13:23:37 -0500

    Ajuste de meta

--- app/head.tsx
+++ app/head.tsx
@@ -1,14 +1,11 @@
 export default function Head() {
   return (
     <>
-      <title>Default Title</title>
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="apple-touch-icon" href="/logo192.png" />
-      <meta name="theme-color" content="#086EAE" />
-      <meta
-        name="description"
-        content="Add description..."
-      />
+      <title>AEQUSST Medicina Preventiva SAS</title>
+      <link rel='icon' href='/favicon.ico' />
+      <link rel='apple-touch-icon' href='/logo192.png' />
+      <meta name='theme-color' content='#086EAE' />
+      <meta name='description' content='AEQUSST Medicina Preventiva SAS' />
     </>
   )
 }
