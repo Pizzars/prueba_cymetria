d3f0920690911715334af4dea1a4b82cf30d3bab
pizzars <jyjdajj@gmail.com>
2023-08-14 16:15:19 -0500

    Ajuste sección app

--- src/screens/app/AppSection.tsx
+++ src/screens/app/AppSection.tsx
@@ -5,7 +5,7 @@ const AppSection = () => {
         width='100%'
         className='h-[100vh] border-none'
         scrolling='auto'
-        src='https://app.biofile.com.co/api/integracionweb/D5g3xyIDpjfVo6jvs3vHadIeKkdeqIaj'
+        src='https://app.biofile.com.co/api/integracionweb/D5g3xyIDpjdzjzsW6I73kQRAh|sZQ3myFyGYni3Ca7Qu2cJVIniSYg=='
       ></iframe>
     </div>
   )
